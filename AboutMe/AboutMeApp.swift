523d75c707e4b470590db08c801714f366974c2f
edhoguntur <edho.guntur@gmail.com>
2023-05-05 22:20:53 +0700

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Edho Guntur Adhitama on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
